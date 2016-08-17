f16e3b8a749f2f90866e9bd5a0e527a2d96ab393
johnngugi <johnngugi777@gmail.com>
2016-08-17 21:43:59 +0300

    Add styling changes to site

--- js/script.js
+++ js/script.js
@@ -10,20 +10,5 @@ $(document).ready(function() {
 
     });
 
-    // var navOffset = $(".top-bar").offset().top;
-    //
-    // $(".top-bar").wrap('<div class="nav-placeholder"></div>');
-    // $(".nav-placeholder").height($(".top-bar").outerHeight);
-    //
-    // $(window).scroll(function() {
-    //     var scrollPos = $(window).scrollTop();
-    //
-    //     if (scrollPos >= navOffset) {
-    //         $(".top-bar").addClass("fixed");
-    //     } else {
-    //         $(".top-bar").removeClass("fixed");
-    //     }
-    // });
-
 
 });
